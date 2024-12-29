367b94e77a9a30b46a04eb197ea0ac76cacdd7f8
RusNB <balushkin-007@mail.ru>
2024-12-29 18:51:55 +0300

    Test

--- src/test/java/ru/netology/postman/PostmanTest.java
+++ src/test/java/ru/netology/postman/PostmanTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+
 class PostmanTest {
     @Test
     void shouldReturnPostTest() {
